de20cfec27804fd378d423dae7edb180d24b1f06
Will Donaldson <donaldson528@gmail.com>
2021-04-13 12:13:53 -0400

    Move build files for website deployment

---
+++ precache-manifest.40adb5f47cdbbd0313600d7e7dec8aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17afa281e5f66bc09964d2d1ad9dfd90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26dc30a907c1da7f12e0",
+    "url": "/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "93db59ba417934a8bea4",
+    "url": "/static/css/main.3f30a9a9.chunk.css"
+  },
+  {
+    "revision": "26dc30a907c1da7f12e0",
+    "url": "/static/js/2.57bff8db.chunk.js"
+  },
+  {
+    "revision": "669c1a02b6b90538804524999a098215",
+    "url": "/static/js/2.57bff8db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93db59ba417934a8bea4",
+    "url": "/static/js/main.35fb846d.chunk.js"
+  },
+  {
+    "revision": "68597826e26b7172e66b",
+    "url": "/static/js/runtime-main.b5d83fcb.js"
+  }
+]);
